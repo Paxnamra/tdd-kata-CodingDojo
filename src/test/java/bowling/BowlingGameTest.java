a0b7e49bdc46cc38d5a779bc9a6652e00fa9e32b
Paxnamra <bialyfeniks4@gmail.com>
2020-03-29 00:23:38 +0100

    expanded tests on scores results

--- src/test/java/bowling/BowlingGameTest.java
+++ src/test/java/bowling/BowlingGameTest.java
@@ -20,8 +20,20 @@ public class BowlingGameTest {
     }
 
     @Test
-    void shouldScoreOneWhenRolledOne() {
-        bowlingGame.roll(1);
-        assertThat(bowlingGame.getScore()).isEqualTo(1);
+    void shouldScore0WhenNoPinDown20Times() {
+        roll(20, 0);
+        assertThat(bowlingGame.getScore()).isEqualTo(0);
+    }
+
+    @Test
+    void shouldScore20WhenPinDown20Times() {
+        roll(20, 1);
+        assertThat(bowlingGame.getScore()).isEqualTo(20);
+    }
+
+    private void roll(int throwsNumber, int pins) {
+        for (int i = pins; i <= throwsNumber; i++) {
+            bowlingGame.roll(pins);
+        }
     }
 }
