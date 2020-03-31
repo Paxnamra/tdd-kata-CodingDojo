2b41b480de30c3ac68e798862f9e95d20531bd76
Paxnamra <bialyfeniks4@gmail.com>
2020-03-31 23:54:56 +0200

    string_calculator kata || green-green empty test start off

---
+++ src/test/java/string_calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package string_calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void shouldReturnZeroWhenGotEmptyString() {
+        assertThat(true).isEqualTo(true);
+    }
+}
