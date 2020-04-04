6ee8392f3ad67b3ef709cdc6dd34583da1b70671
Paxnamra <bialyfeniks4@gmail.com>
2020-04-04 22:08:42 +0200

    string_calculator tests refactor

--- src/test/java/string_calculator/CalculatorTest.java
+++ src/test/java/string_calculator/CalculatorTest.java
@@ -1,22 +1,27 @@
 package string_calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     void shouldReturnZeroWhenGotEmptyString() {
-        Calculator calculator = new Calculator();
-
         int result = calculator.add("");
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     void shouldReturnNumberWhenGivenNumber() {
-        Calculator calculator = new Calculator();
-
         int result = calculator.add("1");
         assertThat(result).isEqualTo(1);
     }
