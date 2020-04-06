3b660749a36591ba69c0e5252789506179b93537
Paxnamra <bialyfeniks4@gmail.com>
2020-04-06 02:25:22 +0200

    string_calculator || refactor

--- src/main/java/string_calculator/Calculator.java
+++ src/main/java/string_calculator/Calculator.java
@@ -6,20 +6,12 @@ public class Calculator {
             return 0;
         }
 
-        if (isSingleNumber(input)) {
-            return Integer.parseInt(input);
-        }
-
-        String[] numbersString = input.split("\\,");
+        String[] numbersString = input.split(",");
 
         int result = 0;
         for (String number : numbersString) {
             result += Integer.parseInt(number);
         }
         return result;
-    }
-
-    private boolean isSingleNumber(String input) {
-        return input.matches("[0-9]");
     }
 }
